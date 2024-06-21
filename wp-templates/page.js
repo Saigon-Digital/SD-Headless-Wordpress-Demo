2e83eff1c34e4e854f3c691c2be9f1d617359084
NguyenQuocThinh13 <thinh@saigon.digital>
2024-06-21 17:07:23 +0700

    feat: add schema markup for page and blogs

--- wp-templates/page.js
+++ wp-templates/page.js
@@ -5,18 +5,20 @@ import { componentsFragment, dynamicBlocks } from '@/fragments/Components'
 import BlocksViewer from '@/functions/BlocksViewer'
 import { gql } from '@apollo/client'
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function Component(props) {
   // Loading state for previews
   if (props.loading) {
     return <>Loading...</>
   }
-  const title = props?.data?.page?.pageSettings?.title || ""
-  const description = props?.data?.page?.pageSettings?.description || ""
-  const canonicalUrl = props?.data?.page?.pageSettings?.canonicalUrl || ""
-  const socialGraphImage = props?.data?.page?.pageSettings?.socialGraphImage 
+  const title = props?.data?.page?.pageSettings?.title || ''
+  const description = props?.data?.page?.pageSettings?.description || ''
+  const canonicalUrl = props?.data?.page?.pageSettings?.canonicalUrl || ''
+  const socialGraphImage = props?.data?.page?.pageSettings?.socialGraphImage
   const noIndex = props?.data?.page?.pageSettings?.noIndex || false
-  const script = props?.data?.page?.pageSettings?.script || ""
+  const script = props?.data?.page?.pageSettings?.script || ''
+  const scriptType = props?.data?.page?.pageSettings?.scriptType || ''
   const pageTitle = props?.data?.page?.title || ''
   const dynamicBlocks = props?.data?.page?.pageBuilder?.dynamicBlocks || []
 
@@ -29,7 +31,11 @@ export default function Component(props) {
         imageUrl={socialGraphImage?.node?.sourceUrl || null}
         noIndex={noIndex}
       />
-      <Script id="page-script">{`${script}`}</Script>
+      <Head>
+        <script type={scriptType} id={`${pageTitle}-script`}>
+          {script}
+        </script>
+      </Head>
       <Header />
       <main>
         <BlocksViewer blocks={dynamicBlocks} />
