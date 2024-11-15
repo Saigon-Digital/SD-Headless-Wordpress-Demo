d8081495db4443645ae86d5570b1b0a28e151add
bigbug_hoangdung <truonghoangdung57@gmail.com>
2024-11-15 13:31:07 +0700

    update

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,25 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(
-  req,
-  res
-) {
-  const { pageSlug } = req.body;
+export default async function handler(req, res) {
+  const { pageSlug } = req.body
 
-  if (req.method === "POST" && pageSlug) {
+  if (req.method === 'POST' && pageSlug) {
     try {
       // Revalidate the specific page based on the slug received
-      await res.revalidate(`/${pageSlug}`);
-      console.log("Revalidated ${pageSlug} =================>", pageSlug);
+      await res.revalidate(`/${pageSlug}`)
+      console.log('Revalidated ${pageSlug} =================>', pageSlug)
       return res.json({
         revalidated: true,
         message: `Revalidated ${pageSlug}`,
-      });
+      })
     } catch (err) {
-      console.log("🚀 ~ err:", err);
-      return res.status(500).json({ error: "Failed to revalidate" });
+      console.log('🚀 ~ err:', err)
+      return res.status(500).json({ error: 'Failed to revalidate', err })
     }
   }
 
-  return res.status(400).json({ error: "Invalid request" });
+  return res.status(400).json({ error: 'Invalid request' })
 }
